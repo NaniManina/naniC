811b0e4ec443ade9d4656a4d787d5e47a16652fa
Нани Манина <118048608+NaniManina@users.noreply.github.com>
2023-02-10 18:15:05 +0500

    ex007
    ex007

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("введите имя пользователя");
-string username = Console.ReadLine();
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
 
-if (username.ToLower() == "маша")
-{
-    Console.WriteLine(" Ура, это же МАША!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+int max = a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+Console.WriteLine($" максимальное число = {max}");
+
